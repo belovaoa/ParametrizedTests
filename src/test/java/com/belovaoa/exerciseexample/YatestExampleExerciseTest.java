44c4ac21bf785039f2a92caa3667ef1286fc74a1
belova_oa <belovva.o@gmail.com>
2022-01-08 19:09:16 +0300

    change testcase.txt and YatestExampleExerciseTest

--- src/test/java/com/belovaoa/exerciseexample/YatestExampleExerciseTest.java
+++ src/test/java/com/belovaoa/exerciseexample/YatestExampleExerciseTest.java
@@ -4,12 +4,14 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class YatestExampleExerciseTest {
 
-    @DisplayName("Поиск в ya.ru слова Selenide")
+    @DisplayName("Поиск на https://yandex.ru/ слова Selenide")
     @Tag("BLOCKER")
     @Test
     void selenideSearchTest() {
@@ -17,6 +19,18 @@ public class YatestExampleExerciseTest {
         $("#text").setValue("Selenide");
         $("button[type='submit']").click();
         $$("li.serp-item").find(Condition.text("Selenide"))
+                .shouldBe(Condition.visible);
+    }
+
+    @ValueSource (strings = {"Selenide", "Allure"})
+    // @DisplayName("Параметризованный тест на поиск на https://yandex.ru/")
+    @Tag("BLOCKER")
+    @ParameterizedTest(name = "Поиск на https://yandex.ru/ слова {0}")
+    void parametrizedSearchYaRuTest(String searchQuery) {
+        open("https://yandex.ru/");
+        $("#text").setValue(searchQuery);
+        $("button[type='submit']").click();
+        $$("li.serp-item").find(Condition.text(searchQuery))
                 .shouldBe(Condition.visible);
     }
 }
